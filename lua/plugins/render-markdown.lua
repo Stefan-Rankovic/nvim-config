67e371ffb0fd23bce9ea9e59660969b1622f924e
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-03-24 17:16:14 +0100

    markdown support

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+-- SPDX-License-Identifier: GPL-3.0-only
+
+-- Renders markdown files (.md) properly.
+--
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {},
+}
